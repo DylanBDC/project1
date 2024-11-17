37ba30f335c12dead21308437ca5e599c667e543
DylanBDC <dylan.brett@dcmail.ca>
2024-11-17 16:52:06 -0500

    Adding the tests for the rest of the coins
    In this commit, I have added the remaining coins to be tested. The tests will fail because I need to return the coins between each test (I will figure out how to add this in my next commit).

--- test_vending_machine_graphical_DB.py
+++ test_vending_machine_graphical_DB.py
@@ -1,5 +1,14 @@
+# Dylan Brett (100933134)
+# TPRG-2131-02
+# Nov 17, 2024
+# This program is strictly my own work. Any material
+# beyond course learning materials that is taken from
+# the Web or other sources is properly cited, giving
+# credit to the original author(s). I havent used any
+# code from other sources other than referncing the course material
+#
 """
-For the 'vending_machine_graphical.py' script - WORKS
+For the 'vending_machine_graphical_DB.py' script - WORKS
 """
 
 from vending_machine_graphical_DB import VendingMachine, WaitingState, AddCoinsState, DeliverProductState, CountChangeState
@@ -7,14 +16,7 @@ from vending_machine_graphical_DB import VendingMachine, WaitingState, AddCoinsS
 def test_VendingMachine():
     # new machine object
     vending = VendingMachine()
-
-    # Add the states - ORG
-    # vending.add_state(WaitingState())
-    # vending.add_state(CoinsState())
-    # vending.add_state(DispenseState())
-    # vending.add_state(ChangeState())
-
-    # My revisions
+    #Adding the states
     vending.add_state(WaitingState())
     vending.add_state(AddCoinsState())
     vending.add_state(DeliverProductState())
@@ -25,7 +27,27 @@ def test_VendingMachine():
     vending.go_to_state('waiting')
     assert vending.state.name == 'waiting'
 
-    # test that the first coin causes a transition to 'coins'
+    # test that the first coin causes a transition to 'coins' and test each coin
+    vending.event = '5' # a nicle
+    vending.update()
+    assert vending.state.name == 'add_coins'
+    assert vending.amount == 5 # pennies, was .total
+    
+    vending.event = '10' # a dime
+    vending.update()
+    assert vending.state.name == 'add_coins'
+    assert vending.amount == 10 # pennies, was .total
+    
+    vending.event = '25' # a quarter
+    vending.update()
+    assert vending.state.name == 'add_coins'
+    assert vending.amount == 25 # pennies, was .total
+    
+    vending.event = '100' # a loonie
+    vending.update()
+    assert vending.state.name == 'add_coins'
+    assert vending.amount == 100 # pennies, was .total
+    
     vending.event = '200' # a twonie
     vending.update()
     assert vending.state.name == 'add_coins'
